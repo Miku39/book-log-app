266fc6532f600b4c4cfa376aae3a18cd1a8e38ed
Miku Sano <miku39.sano@gmail.com>
2020-09-30 08:33:43 +0900

    feat: fetch book list info

---
+++ src/Book.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from "react";
+
+const url = "https://book-log-api.herokuapp.com/books";
+
+function Book() {
+  const [bookListJson, setBookListJson] = useState(null);
+
+  // TODO: async awaitを使用する
+  useEffect(() => {
+    fetch(url)
+      .then((response) => {
+        console.log(response);
+        return response.json();
+      })
+      .then((json) => {
+        console.log(json);
+        setBookListJson(json);
+      });
+  }, []);
+
+  if (!bookListJson) {
+    return <div>Loading</div>;
+  }
+
+  return (
+    <div>
+      {bookListJson.map((bookItem) => {
+        return (
+          <div key={bookItem.isbn}>
+            <div>{bookItem.title}</div>
+            <div>{bookItem.subtitle}</div>
+            <div>
+              <img
+                src={bookItem.imageUrl}
+                alt={bookItem.title}
+                width="128"
+                height="160"
+              ></img>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default Book;
