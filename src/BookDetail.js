314985fec1c9a9073727e4203d59ff7ae950552d
Miku Sano <miku39.sano@gmail.com>
2020-10-05 20:24:07 +0900

    feat: fetch book detail info

--- src/BookDetail.js
+++ src/BookDetail.js
@@ -1,6 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-function BookDetail() {
+const url = "https://book-log-api.herokuapp.com/books";
+
+function BookDetail(props) {
+  const [bookDetailJson, setBookDetailJson] = useState(null);
+  const id = parseInt(props.match.params.id, 10);
+
+  // TODO: async awaitを使用する
+  useEffect(() => {
+    fetch(`${url}/${id}`)
+      .then((response) => {
+        console.log(response);
+        return response.json();
+      })
+      .then((json) => {
+        console.log(json);
+        setBookDetailJson(json);
+      });
+  }, []);
+
+  if (!bookDetailJson) {
+    return <div>Loading</div>;
+  }
+
   return <div>aiueo</div>;
 }
 
