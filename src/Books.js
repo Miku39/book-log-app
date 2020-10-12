5c765beb54ca190b63e3c4627d12534395ff060a
Miku Sano <miku39.sano@gmail.com>
2020-10-12 20:12:14 +0900

    feat(Design):  use makeStyles

--- src/Books.js
+++ src/Books.js
@@ -2,16 +2,10 @@ import React, { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import Grid from "@material-ui/core/Grid";
 import { makeStyles } from "@material-ui/core/styles";
-import Table from "@material-ui/core/Table";
-import TableBody from "@material-ui/core/TableBody";
-import TableCell from "@material-ui/core/TableCell";
-import TableContainer from "@material-ui/core/TableContainer";
-import TableRow from "@material-ui/core/TableRow";
-import Paper from "@material-ui/core/Paper";
 
 const useStyles = makeStyles({
-  table: {
-    minWidth: 650,
+  root: {
+    margin: 16,
   },
 });
 
@@ -37,7 +31,8 @@ function Books() {
   }
 
   return (
-    <div>
+    <div className={classes.root}>
+      <h1>Book List</h1>
       <Grid
         container
         direction="column"
@@ -53,40 +48,17 @@ function Books() {
             key={bookItem.id}
             onClick={() => history.push("/books/" + bookItem.id)}
           >
-            <Grid item xs={2}>
+            <Grid item xs={5}>
               <img src={bookItem.image_url} alt={bookItem.title}></img>
             </Grid>
-            <Grid item xs={6}>
+            <Grid item xs={5}>
               <div>{bookItem.title}</div>
               <div>{bookItem.author}</div>
-            </Grid>
-            <Grid item xs={2}>
-              {bookItem.date}
+              <div>{bookItem.date}</div>
             </Grid>
           </Grid>
         ))}
       </Grid>
-      {/* <TableContainer component={Paper}>
-        <Table className={classes.table}>
-          <TableBody>
-            {bookListJson.map((bookItem) => (
-              <TableRow
-                key={bookItem.id}
-                onClick={() => history.push("/books/" + bookItem.id)}
-              >
-                <TableCell width="30%">
-                  <img src={bookItem.image_url} alt={bookItem.title}></img>
-                </TableCell>
-                <TableCell width="40%">
-                  <div>{bookItem.title}</div>
-                  <div>{bookItem.author}</div>
-                </TableCell>
-                <TableCell width="20%">{bookItem.date}</TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer> */}
     </div>
   );
 }
